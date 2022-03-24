c0731a30eaec8eaa8af20f4df7d90054ce450b51
Jared Garcia <88603841+DjGarcia4@users.noreply.github.com>
2022-03-23 21:58:48 -0600

    Confirmar

--- Empleados/tablas.php
+++ Empleados/tablas.php
@@ -151,8 +151,6 @@
             <div class="input-group">
 
               <input type="text" class="form-control bg-light border-0 small" placeholder=" Buscar..." aria-label="Search" aria-describedby="basic-addon2" name="filtro" id="filtro">
-
-              <input type="text" class="form-control bg-light border-0 small" placeholder="..." aria-label="Search" aria-describedby="basic-addon2" name="filtro" id="filtro">
               <div class="input-group-append">
                 <button class="btn btn-primary" type="button">
                   <i class="fas fa-search fa-sm"></i>
